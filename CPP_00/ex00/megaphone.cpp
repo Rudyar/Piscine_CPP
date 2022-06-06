f6c228db8935a576482b656f408653d73845b191
arudy <arudy@student.42.fr>
2022-06-06 14:17:17 +0200

    MEGAFUN

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,14 +6,30 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 11:33:30 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/06 11:38:46 by arudy            ###   ########.fr       */
+/*   Updated: 2022/06/06 14:11:11 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::cout << "coucouc"  << std::endl;
+	int			i;
+	size_t		j;
+	std::string	str;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (i = 1; i < ac; i++)
+		{
+			str = av[i];
+			for (j = 0; j < str.size(); j++)
+				std::cout << (char)std::toupper(av[i][j]);
+		}
+	}
+	std::cout << std::endl;
 	return (0);
 }
