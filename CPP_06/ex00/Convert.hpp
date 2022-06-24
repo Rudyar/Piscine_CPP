78742ec89a8dc586f5d37a50eb122b939fe94af9
Arthur <arudy@student.42.fr>
2022-06-24 12:34:43 +0200

    cpp06 ex00 wip

--- CPP_06/ex00/Convert.hpp
+++ CPP_06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:35:11 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/24 11:13:11 by arudy            ###   ########.fr       */
+/*   Updated: 2022/06/24 12:29:30 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 #include <limits.h>
 #include <stdlib.h>
 
-void	convertChar(std::string s);
-void	convertInt(char *s);
+void	convertChar(char *s, double n);
+void	convertInt(char *s, double n);
+void	convertFloat(char *s, double n);
 
 #endif
--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -6,30 +6,85 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 14:56:36 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/24 11:25:53 by arudy            ###   ########.fr       */
+/*   Updated: 2022/06/24 12:34:14 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Convert.hpp"
 
-bool	isChar(std::string s) // Pour l'instant check aussi les int < 9
-{
-	if (s.length() > 1)
-		return false;
-	return true;
-}
+// bool	isChar(std::string s) // Pour l'instant check aussi les int < 9
+// {
+// 	if (s.length() > 1)
+// 		return false;
+// 	return true;
+// }
 
-bool isInt(std::string s)
+// bool	isInt(std::string s)
+// {
+// 	size_t i = 0;
+
+// 	if (!isdigit(static_cast<int>(s[i])) && s[i] != '-' && s[i] != '+')
+// 		return false;
+// 	if (s[i] == '-' || s[i] == '+')
+// 		i++;
+// 	for (; i < s.length(); i++)
+// 		if (!isdigit(static_cast<int>(s[i])))
+// 			return false;
+// 	return true;
+// }
+
+// bool	isFloat(std::string s)
+// {
+// 	size_t i = 0;
+
+// 	if (!isdigit(static_cast<int>(s[i])) && s[i] != '-' && s[i] != '+')
+// 		return false;
+// 	if (s[s.length() - 1] != 'f')
+// 		return false;
+// 	if (s[i] == '-' || s[i] == '+')
+// 		i++;
+// 	for (; i < s.length() - 1; i++)
+// 		if (!isdigit(static_cast<int>(s[i])) && s[i] != '.')
+// 			return false;
+// 	return true;
+// }
+
+// bool	isDouble(std::string s)
+// {
+// 	size_t i = 0;
+
+// 	if (!isdigit(static_cast<int>(s[i])) && s[i] != '-' && s[i] != '+')
+// 		return false;
+// 	if (s[i] == '-' || s[i] == '+')
+// 		i++;
+// 	for (; i < s.length() - 1; i++)
+// 		if (!isdigit(static_cast<int>(s[i])) && s[i] != '.')
+// 			return false;
+// 	return true;
+// }
+
+bool	checkInput(std::string s)
 {
 	size_t i = 0;
 
+	if (s.empty())
+		return false;
+	if (s.length() < 2)
+		return true;
 	if (!isdigit(static_cast<int>(s[i])) && s[i] != '-' && s[i] != '+')
+		return false;
+	if (s[s.length() - 1] != 'f' && !isdigit(static_cast<int>(s[s.length() - 1])))
 		return false;
 	if (s[i] == '-' || s[i] == '+')
 		i++;
 	for (; i < s.length(); i++)
-		if (!isdigit(static_cast<int>(s[i])))
-			return false;
+	{
+		if (!isdigit(static_cast<int>(s[i])) && s[i] != '.')
+		{
+			if (i == s.length() - 1 && s[i] != 'f')
+				return false;
+		}
+	}
 	return true;
 }
 
@@ -40,25 +95,23 @@ int main(int ac, char**av)
 		std::cout << "./convert <arg>" << std::endl;
 		return 1;
 	}
-	if (isChar(av[1]))
-		convertChar(av[1]);
-	else if (isInt(av[1]))
-		convertInt(av[1]);
-
-
-
-	/*
-	checker l'input pour voir si c'est un char, un int, un float ou un double et les reste (nan ...)
-	en fonction  du retour, aller dans la bonne function.
-	if is char...
-	Selon moi il faut utiliser les static cast
-	Regarder ce que c'est le std::precision
-
-	./convert a
-	char : 'a'
-	int : 97
-	float : 97.0f
-	double : 97.00
-	*/
+	// Manage exeptions
+	if (checkInput(av[1]) == false)
+	{
+		std::cout << "Wrong input" << std::endl;
+		return 1;
+	}
+	double n = strtod(av[1], NULL);
+	convertChar(av[1], n);
+	convertInt(av[1], n);
+	convertFloat(av[1], n);
+	// if (isChar(av[1]))
+	// 	convertChar(av[1]);
+	// else if (isInt(av[1]))
+	// 	convertInt(av[1]);
+	// else if (isFloat(av[1]))
+	// 	convertFloat(av[1]);
+	// else if (isDouble(av[1]))
+	// 	convertInt(av[1]);
 	return 0;
 }
