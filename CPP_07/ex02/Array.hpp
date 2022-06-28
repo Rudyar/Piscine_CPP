15c8e4df354fa3586b4b5bcd79cea5ec0abd91fa
arudy <arudy@student.42.fr>
2022-06-28 20:26:16 +0200

    cpp07 done

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 17:12:04 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/28 18:24:57 by arudy            ###   ########.fr       */
+/*   Updated: 2022/06/28 20:12:41 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #define ARRAY_HPP
 
 #include <iostream>
-#include <string>
 #include <stdexcept>
 
 template<typename T>
@@ -31,43 +30,53 @@ class Array
 			_array = NULL;
 			_size = 0;
 		};
+
 		Array<T>(unsigned int n)
 		{
 			std::cout << "With size constructor called" << std::endl;
-			if (n < 0)
-				throw(std::length_error("Need a size >= 0"));
+			if (n < 1)
+				throw(std::length_error("Need a size > 0"));
 			_size = n;
 			_array = new T[n];
 		};
+
 		Array<T>(Array<T> const & src)
 		{
-			std::cout << "Copy constructor called" << std::endl;
+			_size = 0;
+			_array = NULL;
 			*this = src;
 		}
-		T &	operator=(T const & rhs)
+
+		~Array<T>()
+		{
+			std::cout << "Destructor called" << std::endl;
+			if (_array)
+				delete [] _array;
+		};
+
+		Array<T> &	operator=(Array<T> const & rhs)
 		{
 			std::cout << "Assignement operator called" << std::endl;
-			if (*this == rhs)
-				return *this;
-			if (this.size() != 0)
-				delete [] _array;
-			_size = rhs._size();
-			for (unsigned int i = 0; i < _size; i++)
-				_array[i] = _array[i];
+			if (this != &rhs)
+			{
+				if (_array)
+					delete [] _array;
+				_size = rhs.size();
+				_array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					_array[i] = rhs._array[i];
+			}
 			return *this;
 		}
+
 		T &	operator[](unsigned int const i)
 		{
 			if (i < 0 || i >= _size)
 				throw(std::out_of_range("Invalid index"));
 			return _array[i];
 		}
-		~Array<T>()
-		{
-			std::cout << "Destructor called" << std::endl;
-			delete [] _array;
-		};
-		unsigned int size()
+
+		unsigned int size() const
 		{
 			return _size;
 		}
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 17:12:48 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/28 18:25:51 by arudy            ###   ########.fr       */
+/*   Updated: 2022/06/28 20:21:03 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,46 @@
 
 int	main(void)
 {
+	std::cout << "---- Create default Array & try to read invalid index ----" << std::endl;
+	Array<int> a;
+	try
+	{
+		std::cout << a[1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl << "---- Create Array of size 10 & add values ----" << std::endl;
+	Array<int>	b(10);
+	for (size_t i = 0; i < 10; i++)
+		b[i] = i;
+	try
+	{
+		std::cout << "Read b[1] : " << b[1] << std::endl;
+		std::cout << "Read b[9] : " << b[9] << std::endl;
+		std::cout << "Read b[10] : " << b[10] << std::endl;
 
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl << "---- Test copy constructor ----" << std::endl;
+	Array<int>	c(b);
+	std::cout << "Array of c" << std::endl;
+	for (size_t i = 0; i < 10; i++)
+		std::cout << "c[i] = " << c[i] << std::endl;
+	std::cout << "Modify value of c" << std::endl;
+	for (size_t i = 0; i < 10; i++)
+		c[i] = i + 1000;
+	std::cout << "Array of c" << std::endl;
+	for (size_t i = 0; i < 10; i++)
+		std::cout << "c[i] = " << c[i] << std::endl;
+	std::cout << "Array of b" << std::endl;
+	for (size_t i = 0; i < 10; i++)
+		std::cout << "b[i] = " << b[i] << std::endl;
+
+	std::cout << std::endl;
 	return 0;
 }
