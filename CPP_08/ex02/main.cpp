b67ea2f9229ca0f740284916b3dd55a58ca4714f
Arthur <arudy@student.42.fr>
2022-07-01 12:00:27 +0200

    cpp08 ex02 need tests

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,11 +6,40 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:02:59 by arudy             #+#    #+#             */
-/*   Updated: 2022/06/30 18:03:11 by arudy            ###   ########.fr       */
+/*   Updated: 2022/07/01 11:54:53 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "MutantStack.hpp"
+
+int main()
 {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
 	return 0;
 }
